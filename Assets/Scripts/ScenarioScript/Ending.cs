2779e27ca1e398642d7d9bb3f4b832835e945c70
Zhenglin234 <zyang352@usc.edu>
2025-02-12 19:07:09 -0800

    ball

---
+++ Assets/Scripts/ScenarioScript/Ending.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending : MonoBehaviour
+{
+    private bool isEnding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        Debug.Log("Ending OnTriggerEnter2D");
+
+        if (isEnding)
+        {
+            return;
+        }
+
+        if (collision.gameObject.tag != "Player")
+        {
+            return;
+        }
+
+        isEnding = true;
+
+        collision.gameObject.SetActive(false);
+
+        ScenarioSpin scenarioSpin = FindObjectOfType<ScenarioSpin>();
+        if (scenarioSpin != null)
+        {
+            scenarioSpin.DisableRotation();
+        }
+    }
+
+}
